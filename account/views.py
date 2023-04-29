ff2ee1b6a39a5d060c3143909b26faf534f4e4a6
m.borbor <bourbour619@gamil.com>
2023-04-29 19:50:55 +0330

    register user redirect after changed

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from core.models import User
 from django.views.generic import FormView
 from .forms import *
@@ -8,12 +9,23 @@ class RegisterView(FormView):
     form_class = RegisterAccountForm
     template_name = 'account/register.html'
     success_url = '/account/login/'
+    
+
 
     def form_valid(self, form):
         user = form.save(commit=False)
         user.type = User.UserType('STAFF')
         user.save()
+        self.username = user.username
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        url = self.success_url
+        if self.username:
+            url += f'?username={self.username}'
+        return url
+
+    
 
             
 
@@ -21,6 +33,13 @@ class LoginView(FormView):
     form_class = LoginAccountForm
     template_name = 'account/login.html'
     success_url = '/account/profile/'
+
+    def get_initial(self):
+        form_initial = super().get_initial()
+        if self.request.method == 'GET':
+            username = self.request.GET.get('username')
+            form_initial['username'] = username
+        return form_initial
 
 
 
